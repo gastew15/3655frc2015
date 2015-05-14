5584deb94b75a4709388d0c404eca66e2a795ed1
G. Stewart <gastewserver@hotmail.com>
2015-05-14 07:51:54 -0400

    Added in some helper methods to the teleopmapping class

--- RobotCode/src/org/usfirst/frc/team3655/tools/TeleOpAutonMapping.java
+++ RobotCode/src/org/usfirst/frc/team3655/tools/TeleOpAutonMapping.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team3655.tools;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.wpi.first.wpilibj.Joystick;
 
 /**
@@ -10,7 +13,9 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class TeleOpAutonMapping 
 {
-	Joystick joystick;
+	private Joystick joystick;
+	private boolean[] currentButtons;
+	private boolean[] previousButtons;
 	
 	/**
 	 * Base constructor that accepts what joystick to monitor
@@ -23,6 +28,31 @@ public class TeleOpAutonMapping
 	
 	public void startMapping()
 	{
+		//Loop for running
+			//Get buttons changed and as they do have the times down
 		
+		//End of loop set old joystick to current
+	}
+	
+	private boolean[] getButtonStates(Joystick joystick)
+	{
+		boolean[] buttonStates = new boolean[joystick.getButtonCount()];
+		for(int i = 0; i < buttonStates.length; i++)
+		{
+			buttonStates[i] = joystick.getRawButton(i);
+		}
+		return buttonStates;
+	}
+	
+	private List<Integer> getButtonsChanged(boolean[] currentButtons, boolean[] previousButtons)
+	{
+		List<Integer> changed = new ArrayList<Integer>();
+		
+		for(int i = 0; i < currentButtons.length; i++)
+		{
+			if(currentButtons[i] != previousButtons[i])
+				changed.add(i);
+		}
+		return changed;
 	}
 }
